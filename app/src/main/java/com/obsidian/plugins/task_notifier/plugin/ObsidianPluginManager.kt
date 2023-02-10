7e7470c1ddb078418578ffbf39d84682751e4e52
moqs <moqs001@gmail.com>
2023-02-10 18:10:59 +0100

    working file observing. parsing the fucked up format still sucks

--- app/src/main/java/com/obsidian/plugins/task_notifier/plugin/ObsidianPluginManager.kt
+++ app/src/main/java/com/obsidian/plugins/task_notifier/plugin/ObsidianPluginManager.kt
@@ -2,8 +2,11 @@ package com.obsidian.plugins.task_notifier.plugin
 
 import android.net.Uri
 import com.google.gson.Gson
+import com.google.gson.internal.LinkedTreeMap
+import com.obsidian.plugins.task_notifier.plugin.dto.v1.ObsidianReminderPluginActualReminderDTO
 import com.obsidian.plugins.task_notifier.plugin.dto.v1.ObsidianReminderPluginConfigDTO
 import com.obsidian.plugins.task_notifier.utils.Logger
+import java.text.SimpleDateFormat
 import java.util.Date
 
 class ObsidianPluginManager {
@@ -28,13 +31,25 @@ class ObsidianPluginManager {
             var result: List<ObsidianReminderBO> = emptyList();
             val gson = Gson()
             var obsidianConfig = gson.fromJson(json, ObsidianReminderPluginConfigDTO::class.java)
-            obsidianConfig.reminders.forEach { reminder ->
-                var titleCandidate = "N/A"
-                if (reminder.title != null) {
-                    titleCandidate = reminder.title.toString()
+
+            val jsonObject = Gson().fromJson(json, com.google.gson.JsonObject::class.java)
+            val remindersObject = jsonObject.getAsJsonObject("reminders")
+            for ((key, value) in remindersObject.entrySet()) {
+                val reminderArray = value.asJsonArray
+                for (i in 0 until reminderArray.size()) {
+                    val reminder = reminderArray[i].asJsonObject
+                    val title = reminder.get("title").asString
+                    val time = reminder.get("time").asString
+                    val simpleDateFormat =
+                        SimpleDateFormat(obsidianConfig.settings!!.dateTimeFormat)
+                    var date: Date? = null
+                    try {
+                        date = simpleDateFormat.parse(time)
+                    } catch (e: java.lang.Exception) {
+                    }
+                    if (date == null) continue
+                    result.plus(ObsidianReminderBO(title, date))
                 }
-                var timeCandidate = Date(reminder.time)
-                result.plus(ObsidianReminderBO(titleCandidate, timeCandidate))
             }
             return result;
         }
