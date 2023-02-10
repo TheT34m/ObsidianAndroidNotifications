eddddb697fe5ede3f6ba4e67c01c05b95e3b2823
moqs <moqs001@gmail.com>
2023-02-10 17:01:47 +0100

    trying to make work file watcher

--- app/src/main/java/com/obsidian/plugins/task_notifier/os/FIleObserverService.kt
+++ app/src/main/java/com/obsidian/plugins/task_notifier/os/FIleObserverService.kt
@@ -12,36 +12,47 @@ import com.obsidian.plugins.task_notifier.core.OnFileChangedResult
 import com.obsidian.plugins.task_notifier.utils.Logger
 
 import java.io.BufferedReader
+import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
 
 class FileObserverService : Service() {
     private var mFileObserver: FileObserver? = null
+
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Logger.info("FileObserverService.onStartCommand")
         val context: Context = this
-        val path = intent.getStringExtra(ServiceManager.FILE_PATH_INTENT_KEY)
-        if (path == null) {
+        val uriString = intent.getStringExtra(ServiceManager.FILE_PATH_INTENT_KEY)
+        if (uriString == null) {
             Logger.info("FileObserverService missing path from FILE_PATH_INTENT_KEY intent key")
             return STOP_FOREGROUND_REMOVE;
         }
-        Logger.info("FileObserverService started for path $path")
+        Logger.info("FileObserverService started for uri ${uriString}")
+        val uri = Uri.parse(uriString);
 
-        mFileObserver = getFileObserver(context, path)
+        mFileObserver = getFileObserver(context, uriString)
         (mFileObserver as FileObserver).startWatching() // The FileObserver starts watching
-        return START_NOT_STICKY
+        return START_STICKY
     }
 
     override fun onBind(p0: Intent?): IBinder? {
+        Logger.info("FileObserverService.onBind")
         TODO("Not yet implemented")
     }
 
     override fun onCreate() {
         super.onCreate()
+        Logger.info("FileObserverService.onCreate")
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        Logger.info("FileObserverService.onDestroy")
+    }
 
-    private fun getFileObserver(context: Context, path: String): FileObserver {
-        return object : FileObserver(path) {
+    private fun getFileObserver(context: Context, uri: String): FileObserver {
+        Logger.info("FileObserverService created for file: ${uri} ")
+        return object : FileObserver(uri, ALL_EVENTS) {
             override fun onEvent(event: Int, path: String?) {
                 Logger.info("FileObserverService.onEvent")
                 if (path == null) return
